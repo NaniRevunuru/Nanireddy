fae24a9ee1edd3fe2e0da71cff88b7d86df51a01
NaniNextzen <122022368+NaniRevunuru@users.noreply.github.com>
2023-11-23 11:10:31 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -12,23 +12,24 @@ class Employee:
 
     def calculate_performance(self):
         # Perform some calculations to determine performance rating (for illustration purposes)
-        if self.salary > 50000:
+        if self.salary > 70000:
             self.performance_rating += 2
-        if self.age < 30:
+        if self.age < 35:
             self.performance_rating += 1
+        # Add more criteria as needed for a specific company
 
     def display_info(self):
-        return f"Name: {self.name}\nAge: {self.age}\nPosition: {self.position}\nSalary: {self.salary}\nPerformance Rating: {self.performance_rating}"
+        return f"Name: {self.name}\nAge: {self.age}\nPosition: {self.position}\nSalary: ${self.salary:,.2f}\nPerformance Rating: {self.performance_rating}"
 
 # Streamlit UI
 def main():
-    st.title("Employee Performance Calculator")
+    st.title("Employee Performance Calculator (MNC)")
 
     # Get employee details from the user
     name = st.text_input("Enter Employee Name:")
     age = st.number_input("Enter Employee Age:")
     position = st.text_input("Enter Employee Position:")
-    salary = st.number_input("Enter Employee Salary:")
+    salary = st.number_input("Enter Employee Salary ($):")
 
     # Create an instance of the Employee class
     employee = Employee(name, age, position, salary)
